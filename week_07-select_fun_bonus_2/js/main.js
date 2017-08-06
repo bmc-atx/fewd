8472ee03392d08482de1f421a1d90ff3cb65aea8
Brewster McCracken <mccrackenb@me.com>
2017-08-06 07:54:02 -0700

    finished week 7 homework
    finished select_fun_bonus_2

--- week_07-select_fun_bonus_2/js/main.js
+++ week_07-select_fun_bonus_2/js/main.js
@@ -1,22 +1,34 @@
-// 1.	When the header element is clicked
+// DONE 1.	When the header element is clicked
   // a. Slide up the header
+  $('header').on('click', function () {
+    $('header').slideUp(350);
+  });
 
 
 // 2. When the "Throw a party" button is clicked
-  // a. Remove the boring class from the section with the id boxes
-  // b. Fade out all of the divs in #row4 slowly (hint: your selector could be $('#row4 div') )
-  // c. Add a class snazzy in your css and add it to all items with the class .box
-  // c. Change the styling of the span inside of #box3 by adding a class fun (and style that fun class in CSS)
-  // SlideUp all the h2s on the page quickly
-  // Add the selected class [already in the CSS] to the divs in #row3
+  // DONE a. Remove the boring class from the section with the id boxes
+  // DONE b. Fade out all of the divs in #row4 slowly (hint: your selector could be $('#row4 div') )
+  //DONE c. Add a class snazzy in your css and add it to all items with the class .box
+  // DONE c. Change the styling of the span inside of #box3 by adding a class fun (and style that fun class in CSS)
+  //DONE SlideUp all the h2s on the page quickly
+  // DONE Add the selected class [already in the CSS] to the divs in #row3
   // BONUS: Add a paragraph that says "Time to party!!!" to all boxes with class .add-para (Hint: you'll need to look up the append() method)
   // BONUS: Add an h4 that says "Wild!" to the beginning of #box12 (Hint: you'll need to look up the prepend() method)
 
-
+$("#party-time").on('click', function () {
+  $('#boxes').removeClass('boring');
+  $('#row4 div').fadeOut('slow');
+  $('.box').addClass('snazzy');
+  $('#box3 span').addClass('fun');
+  $('h2').slideUp(300);
+  $('#row3 div').addClass('selected');
+});
 
 // 3. When the anchor in #box2 is clicked (hint: your selector could be $('#box2 a') )
-  // a. Fade in the footer slowly
-
+  //DONE a. Fade in the footer slowly
+$('#box2 a').on('click', function () {
+  $('footer').fadeIn('slow');
+});
 
 // 4. When the "Party Pooper" button is clicked
   // a. Slide down the header
@@ -30,3 +42,13 @@
   // BONUS: If you completed the above bonus tasks, look up "remove jquery"
   // and then see if you can remove the paragraphs and h3s that you added in.
   // make sure to ONLY remove those paragraphs and h3s you added, not all the paragraphs and h3s!
+$('#reset').on('click', function () {
+  $('header').slideDown(350);
+  $('footer').fadeOut('slow');
+  $('#row4 div').show();
+  $('#boxes').addClass('boring');
+  $('.box').removeClass('snazzy');
+  $('#box3 span').removeClass('fun');
+  $('#row3 div').removeClass('selected');
+  $('h2').slideDown(300);
+});
